9ac66a759895af95834afd7317bc87a5be4ec330
Kristofer Tengström <info@kristofer-tengstrom.se>
2018-08-12 11:30:46 +0200

    kristofer: Simplified config syntax

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 
 config :logical_permissions, :bypass_access_checker, LogicalPermissions.Test.BypassAccess
-config :logical_permissions, :permission_types, [flag: LogicalPermissions.Test.Flag]
-config :logical_permissions, :permission_types, [role: LogicalPermissions.Test.Role]
+config :logical_permissions, :permission_types, flag: LogicalPermissions.Test.Flag
+config :logical_permissions, :permission_types, role: LogicalPermissions.Test.Role
